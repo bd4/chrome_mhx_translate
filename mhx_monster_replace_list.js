ceac6ce79efa4a897df299d208a479a1c39537b8
Bryce Allen <ballen@ci.uchicago.edu>
2015-12-05 11:00:39 -0600

    add monster link titles for materials

---
+++ mhx_monster_replace_list.js
@@ -0,0 +1,714 @@
+monster_replace_list = [
+  [
+    "\u7ffc\u86c7\u7adc", 
+    "Remobra (\u30ac\u30d6\u30e9\u30b9)"
+  ], 
+  [
+    "\u77db\u7815\u30c0\u30a4\u30df\u30e7\u30a6\u30b6\u30b6\u30df", 
+    "Spearbreaker Daimyo Hermitaur (\u77db\u7815\u30c0\u30a4\u30df\u30e7\u30a6\u30b6\u30b6\u30df)"
+  ], 
+  [
+    "\u30c7\u30c3\u30c9\u30ea\u30fc\u30d7\u30ea\u30f3\u30bb\u30b9", 
+    "Violet Poison Princess Rathian (\u7d2b\u6bd2\u59eb\u30ea\u30aa\u30ec\u30a4\u30a2)"
+  ], 
+  [
+    "\u8352\u9264\u722a\u30c6\u30a3\u30ac\u30ec\u30c3\u30af\u30b9", 
+    "Ruinous Hook Claw Tigrex (\u8352\u9264\u722a\u30c6\u30a3\u30ac\u30ec\u30c3\u30af\u30b9)"
+  ], 
+  [
+    "\u30af\u30ea\u30e0\u30be\u30f3\u30fb\u30d9\u30a2\u30fc", 
+    "Crimson Helmet Arzuros (\u7d05\u515c\u30a2\u30aa\u30a2\u30b7\u30e9)"
+  ], 
+  [
+    "\u71fc\u6ec5\u5203\u30c7\u30a3\u30ce\u30d0\u30eb\u30c9", 
+    "Incinerating Blade Dinovaldo (\u71fc\u6ec5\u5203\u30c7\u30a3\u30ce\u30d0\u30eb\u30c9)"
+  ], 
+  [
+    "\u767d\u75be\u98a8\u30ca\u30eb\u30ac\u30af\u30eb\u30ac", 
+    "White Gale Nargacuga (\u767d\u75be\u98a8\u30ca\u30eb\u30ac\u30af\u30eb\u30ac)"
+  ], 
+  [
+    "\u96bb\u773c\u30a4\u30e3\u30f3\u30ac\u30eb\u30eb\u30ac", 
+    "One-Eyed Yian Garuga (\u96bb\u773c\u30a4\u30e3\u30f3\u30ac\u30eb\u30eb\u30ac)"
+  ], 
+  [
+    "\u30c0\u30a4\u30df\u30e7\u30a6\u30b6\u30b6\u30df", 
+    "Daimyo Hermitaur (\u30c0\u30a4\u30df\u30e7\u30a6\u30b6\u30b6\u30df)"
+  ], 
+  [
+    "\u5927\u96ea\u4e3b\u30a6\u30eb\u30af\u30b9\u30b9", 
+    "Heavy Snow Lord Lagombi (\u5927\u96ea\u4e3b\u30a6\u30eb\u30af\u30b9\u30b9)"
+  ], 
+  [
+    "\u30c0\u30fc\u30af\u30cd\u30b9\u30ed\u30fc\u30c9", 
+    "Black Flame King Rathalos (\u9ed2\u708e\u738b\u30ea\u30aa\u30ec\u30a6\u30b9)"
+  ], 
+  [
+    "\u9ed2\u708e\u738b\u30ea\u30aa\u30ec\u30a6\u30b9", 
+    "Black Flame King Rathalos (\u9ed2\u708e\u738b\u30ea\u30aa\u30ec\u30a6\u30b9)"
+  ], 
+  [
+    "\u30ea\u30aa\u30ec\u30a6\u30b9\u5e0c\u5c11\u7a2e", 
+    "Silver Rathalos (\u30ea\u30aa\u30ec\u30a6\u30b9\u5e0c\u5c11\u7a2e)"
+  ], 
+  [
+    "\u7d2b\u6bd2\u59eb\u30ea\u30aa\u30ec\u30a4\u30a2", 
+    "Violet Poison Princess Rathian (\u7d2b\u6bd2\u59eb\u30ea\u30aa\u30ec\u30a4\u30a2)"
+  ], 
+  [
+    "\u30ea\u30aa\u30ec\u30a4\u30a2\u5e0c\u5c11\u7a2e", 
+    "Gold Rathian (\u30ea\u30aa\u30ec\u30a4\u30a2\u5e0c\u5c11\u7a2e)"
+  ], 
+  [
+    "\u30b2\u30cd\u30eb\u30fb\u30bb\u30eb\u30bf\u30b9", 
+    "Seltas Queen (\u30b2\u30cd\u30eb\u30fb\u30bb\u30eb\u30bf\u30b9)"
+  ], 
+  [
+    "\u30b7\u30e7\u30a6\u30b0\u30f3\u30ae\u30b6\u30df", 
+    "Shogun Ceanataur (\u30b7\u30e7\u30a6\u30b0\u30f3\u30ae\u30b6\u30df)"
+  ], 
+  [
+    "\u5b9d\u7e8f\u30a6\u30e9\u30ac\u30f3\u30ad\u30f3", 
+    "Treasure Clad Uragaan (\u5b9d\u7e8f\u30a6\u30e9\u30ac\u30f3\u30ad\u30f3)"
+  ], 
+  [
+    "\u91d1\u96f7\u516c\u30b8\u30f3\u30aa\u30a6\u30ac", 
+    "Golden Thunder Prince Zinogre (\u91d1\u96f7\u516c\u30b8\u30f3\u30aa\u30a6\u30ac)"
+  ], 
+  [
+    "\u30c6\u30aa\u30fb\u30c6\u30b9\u30ab\u30c8\u30eb", 
+    "Teostra (\u30c6\u30aa\u30fb\u30c6\u30b9\u30ab\u30c8\u30eb)"
+  ], 
+  [
+    "\u7d05\u515c\u30a2\u30aa\u30a2\u30b7\u30e9", 
+    "Crimson Helmet Arzuros (\u7d05\u515c\u30a2\u30aa\u30a2\u30b7\u30e9)"
+  ], 
+  [
+    "\u30d6\u30e9\u30ad\u30c7\u30a3\u30aa\u30b9", 
+    "Brachydios (\u30d6\u30e9\u30ad\u30c7\u30a3\u30aa\u30b9)"
+  ], 
+  [
+    "\u30ac\u30e9\u30e9\u30a2\u30b8\u30e3\u30e9", 
+    "Najarala (\u30ac\u30e9\u30e9\u30a2\u30b8\u30e3\u30e9)"
+  ], 
+  [
+    "\u60aa\u76f8\u306e\u5343\u4e21\u5f79\u8005", 
+    "Rock Piercing Tetsucabra (\u5ca9\u7a7f\u30c6\u30c4\u30ab\u30d6\u30e9)"
+  ], 
+  [
+    "\u5ca9\u7a7f\u30c6\u30c4\u30ab\u30d6\u30e9", 
+    "Rock Piercing Tetsucabra (\u5ca9\u7a7f\u30c6\u30c4\u30ab\u30d6\u30e9)"
+  ], 
+  [
+    "\u30c6\u30a3\u30ac\u30ec\u30c3\u30af\u30b9", 
+    "Tigrex (\u30c6\u30a3\u30ac\u30ec\u30c3\u30af\u30b9)"
+  ], 
+  [
+    "\u30a4\u30e3\u30f3\u30ac\u30eb\u30eb\u30ac", 
+    "Yian Garuga (\u30a4\u30e3\u30f3\u30ac\u30eb\u30eb\u30ac)"
+  ], 
+  [
+    "\u30a2\u30eb\u30d0\u30c8\u30ea\u30aa\u30f3", 
+    "Alatreon (\u30a2\u30eb\u30d0\u30c8\u30ea\u30aa\u30f3)"
+  ], 
+  [
+    "\u30a2\u30de\u30c4\u30de\u30ac\u30c4\u30c1", 
+    "Amatsumagatsuchi (\u30a2\u30de\u30c4\u30de\u30ac\u30c4\u30c1)"
+  ], 
+  [
+    "\u30af\u30b7\u30e3\u30eb\u30c0\u30aa\u30e9", 
+    "Kushala Daora (\u30af\u30b7\u30e3\u30eb\u30c0\u30aa\u30e9)"
+  ], 
+  [
+    "\u30b7\u30e3\u30ac\u30eb\u30de\u30ac\u30e9", 
+    "Shagaru Magala (\u30b7\u30e3\u30ac\u30eb\u30de\u30ac\u30e9)"
+  ], 
+  [
+    "\u30a2\u30b0\u30ca\u30b3\u30c8\u30eb", 
+    "Agnaktor (\u30a2\u30b0\u30ca\u30b3\u30c8\u30eb)"
+  ], 
+  [
+    "\u30a2\u30ab\u30e0\u30c8\u30eb\u30e0", 
+    "Akantor (\u30a2\u30ab\u30e0\u30c8\u30eb\u30e0)"
+  ], 
+  [
+    "\u30c9\u30c9\u30d6\u30e9\u30f3\u30b4", 
+    "Blangonga (\u30c9\u30c9\u30d6\u30e9\u30f3\u30b4)"
+  ], 
+  [
+    "\u30c9\u30b9\u30d5\u30a1\u30f3\u30b4", 
+    "Bulldrome (\u30c9\u30b9\u30d5\u30a1\u30f3\u30b4)"
+  ], 
+  [
+    "\u30c9\u30b9\u30ac\u30ec\u30aa\u30b9", 
+    "Cephadrome (\u30c9\u30b9\u30ac\u30ec\u30aa\u30b9)"
+  ], 
+  [
+    "\u30a4\u30d3\u30eb\u30b8\u30e7\u30fc", 
+    "Deviljho (\u30a4\u30d3\u30eb\u30b8\u30e7\u30fc)"
+  ], 
+  [
+    "\u30c7\u30a3\u30ce\u30d0\u30eb\u30c9", 
+    "Dinovaldo (\u30c7\u30a3\u30ce\u30d0\u30eb\u30c9)"
+  ], 
+  [
+    "\u30c9\u30dc\u30eb\u30d9\u30eb\u30af", 
+    "Duramboros (\u30c9\u30dc\u30eb\u30d9\u30eb\u30af)"
+  ], 
+  [
+    "\u30c9\u30b9\u30b2\u30cd\u30dd\u30b9", 
+    "Gendrome (\u30c9\u30b9\u30b2\u30cd\u30dd\u30b9)"
+  ], 
+  [
+    "\u30b4\u30a2\u30fb\u30de\u30ac\u30e9", 
+    "Gore Magala (\u30b4\u30a2\u30fb\u30de\u30ac\u30e9)"
+  ], 
+  [
+    "\u30c9\u30b9\u30de\u30c3\u30ab\u30a9", 
+    "Great Maccau (\u30c9\u30b9\u30de\u30c3\u30ab\u30a9)"
+  ], 
+  [
+    "\u30db\u30ed\u30ed\u30db\u30eb\u30eb", 
+    "Hororohoruru (\u30db\u30ed\u30ed\u30db\u30eb\u30eb)"
+  ], 
+  [
+    "\u30c9\u30b9\u30a4\u30fc\u30aa\u30b9", 
+    "Iodrome (\u30c9\u30b9\u30a4\u30fc\u30aa\u30b9)"
+  ], 
+  [
+    "\u30b1\u30c1\u30e3\u30ef\u30c1\u30e3", 
+    "Kecha Wacha (\u30b1\u30c1\u30e3\u30ef\u30c1\u30e3)"
+  ], 
+  [
+    "\u30e9\u30ae\u30a2\u30af\u30eb\u30b9", 
+    "Lagiacrus (\u30e9\u30ae\u30a2\u30af\u30eb\u30b9)"
+  ], 
+  [
+    "\u30f4\u30a9\u30eb\u30ac\u30ce\u30b9", 
+    "Lavasioth (\u30f4\u30a9\u30eb\u30ac\u30ce\u30b9)"
+  ], 
+  [
+    "\u30ca\u30eb\u30ac\u30af\u30eb\u30ac", 
+    "Nargacuga (\u30ca\u30eb\u30ac\u30af\u30eb\u30ac)"
+  ], 
+  [
+    "\u30cf\u30d7\u30eb\u30dc\u30c3\u30ab", 
+    "Nibelsnarf (\u30cf\u30d7\u30eb\u30dc\u30c3\u30ab)"
+  ], 
+  [
+    "\u30ed\u30a2\u30eb\u30c9\u30ed\u30b9", 
+    "Royal Ludroth (\u30ed\u30a2\u30eb\u30c9\u30ed\u30b9)"
+  ], 
+  [
+    "\u30a2\u30eb\u30bb\u30eb\u30bf\u30b9", 
+    "Seltas (\u30a2\u30eb\u30bb\u30eb\u30bf\u30b9)"
+  ], 
+  [
+    "\u30bb\u30eb\u30ec\u30ae\u30aa\u30b9", 
+    "Seregios (\u30bb\u30eb\u30ec\u30ae\u30aa\u30b9)"
+  ], 
+  [
+    "\u30a6\u30ab\u30e0\u30eb\u30d0\u30b9", 
+    "Ukanlos (\u30a6\u30ab\u30e0\u30eb\u30d0\u30b9)"
+  ], 
+  [
+    "\u30c9\u30b9\u30e9\u30f3\u30dd\u30b9", 
+    "Velocidrome (\u30c9\u30b9\u30e9\u30f3\u30dd\u30b9)"
+  ], 
+  [
+    "\u30e9\u30f3\u30b0\u30ed\u30c8\u30e9", 
+    "Volvidon (\u30e9\u30f3\u30b0\u30ed\u30c8\u30e9)"
+  ], 
+  [
+    "\u30a4\u30e3\u30f3\u30af\u30c3\u30af", 
+    "Yian Kut-Ku (\u30a4\u30e3\u30f3\u30af\u30c3\u30af)"
+  ], 
+  [
+    "\u30b6\u30dc\u30a2\u30b6\u30ae\u30eb", 
+    "Zamtrios (\u30b6\u30dc\u30a2\u30b6\u30ae\u30eb)"
+  ], 
+  [
+    "\u30aa\u30b9\u30c8\u30ac\u30ed\u30a2", 
+    "Osutogaroa (\u30aa\u30b9\u30c8\u30ac\u30ed\u30a2)"
+  ], 
+  [
+    "\u30b8\u30e3\u30ae\u30a3\u30ce\u30b9", 
+    "Jaggia (\u30b8\u30e3\u30ae\u30a3\u30ce\u30b9)"
+  ], 
+  [
+    "\u30d6\u30eb\u30d5\u30a1\u30f3\u30b4", 
+    "Bullfango (\u30d6\u30eb\u30d5\u30a1\u30f3\u30b4)"
+  ], 
+  [
+    "\u30a2\u30aa\u30a2\u30b7\u30e9", 
+    "Arzuros (\u30a2\u30aa\u30a2\u30b7\u30e9)"
+  ], 
+  [
+    "\u30a6\u30eb\u30af\u30b9\u30b9", 
+    "Lagombi (\u30a6\u30eb\u30af\u30b9\u30b9)"
+  ], 
+  [
+    "\u98a8\u65ac\u308b\u767d\u5f71", 
+    "White Gale Nargacuga (\u767d\u75be\u98a8\u30ca\u30eb\u30ac\u30af\u30eb\u30ac)"
+  ], 
+  [
+    "\u30ac\u30ce\u30c8\u30c8\u30b9", 
+    "Plesioth (\u30ac\u30ce\u30c8\u30c8\u30b9)"
+  ], 
+  [
+    "\u30e9\u30a4\u30bc\u30af\u30b9", 
+    "Raizekusu (\u30e9\u30a4\u30bc\u30af\u30b9)"
+  ], 
+  [
+    "\u30ea\u30aa\u30ec\u30a6\u30b9", 
+    "Rathalos (\u30ea\u30aa\u30ec\u30a6\u30b9)"
+  ], 
+  [
+    "\u30ea\u30aa\u30ec\u30a4\u30a2", 
+    "Rathian (\u30ea\u30aa\u30ec\u30a4\u30a2)"
+  ], 
+  [
+    "\u30bf\u30de\u30df\u30c4\u30cd", 
+    "Tamamitsune (\u30bf\u30de\u30df\u30c4\u30cd)"
+  ], 
+  [
+    "\u30c6\u30c4\u30ab\u30d6\u30e9", 
+    "Tetsucabra (\u30c6\u30c4\u30ab\u30d6\u30e9)"
+  ], 
+  [
+    "\u30b8\u30f3\u30aa\u30a6\u30ac", 
+    "Zinogre (\u30b8\u30f3\u30aa\u30a6\u30ac)"
+  ], 
+  [
+    "\u771f\u306a\u308b\u899a\u9192", 
+    "Golden Thunder Prince Zinogre (\u91d1\u96f7\u516c\u30b8\u30f3\u30aa\u30a6\u30ac)"
+  ], 
+  [
+    "\u30aa\u30aa\u30ca\u30ba\u30c1", 
+    "Chameleos (\u30aa\u30aa\u30ca\u30ba\u30c1)"
+  ], 
+  [
+    "\u30b9\u30af\u30a2\u30ae\u30eb", 
+    "Zamite (\u30b9\u30af\u30a2\u30ae\u30eb)"
+  ], 
+  [
+    "\u30a2\u30d7\u30b1\u30ed\u30b9", 
+    "Apceros (\u30a2\u30d7\u30b1\u30ed\u30b9)"
+  ], 
+  [
+    "\u30a2\u30d7\u30c8\u30ce\u30b9", 
+    "Aptonoth (\u30a2\u30d7\u30c8\u30ce\u30b9)"
+  ], 
+  [
+    "\u30ea\u30ce\u30d7\u30ed\u30b9", 
+    "Rhenoplos (\u30ea\u30ce\u30d7\u30ed\u30b9)"
+  ], 
+  [
+    "\u30ea\u30e2\u30bb\u30c8\u30b9", 
+    "Rimosetosu (\u30ea\u30e2\u30bb\u30c8\u30b9)"
+  ], 
+  [
+    "\u30ba\u30ef\u30ed\u30dd\u30b9", 
+    "Slagtoth (\u30ba\u30ef\u30ed\u30dd\u30b9)"
+  ], 
+  [
+    "\u30a6\u30ed\u30b3\u30c8\u30eb", 
+    "Uroktor (\u30a6\u30ed\u30b3\u30c8\u30eb)"
+  ], 
+  [
+    "\u30aa\u30eb\u30bf\u30ed\u30b9", 
+    "Altaroth (\u30aa\u30eb\u30bf\u30ed\u30b9)"
+  ], 
+  [
+    "\u30d6\u30ca\u30cf\u30d6\u30e9", 
+    "Bnahabra (\u30d6\u30ca\u30cf\u30d6\u30e9)"
+  ], 
+  [
+    "\u30ab\u30f3\u30bf\u30ed\u30b9", 
+    "Hornetaur (\u30ab\u30f3\u30bf\u30ed\u30b9)"
+  ], 
+  [
+    "\u30af\u30f3\u30c1\u30e5\u30a6", 
+    "Konchu (\u30af\u30f3\u30c1\u30e5\u30a6)"
+  ], 
+  [
+    "\u30e9\u30f3\u30b4\u30b9\u30bf", 
+    "Vespoid (\u30e9\u30f3\u30b4\u30b9\u30bf)"
+  ], 
+  [
+    "\u30ac\u30e0\u30fc\u30c8", 
+    "Gamuto (\u30ac\u30e0\u30fc\u30c8)"
+  ], 
+  [
+    "\u30b2\u30ea\u30e7\u30b9", 
+    "Gypceros (\u30b2\u30ea\u30e7\u30b9)"
+  ], 
+  [
+    "\u5e2f\u96fb\u98db\u7adc", 
+    "Khezu (\u30d5\u30eb\u30d5\u30eb)"
+  ], 
+  [
+    "\u30d5\u30eb\u30d5\u30eb", 
+    "Khezu (\u30d5\u30eb\u30d5\u30eb)"
+  ], 
+  [
+    "\u30ac\u30fc\u30b0\u30a1", 
+    "Gargwa (\u30ac\u30fc\u30b0\u30a1)"
+  ], 
+  [
+    "\u30ae\u30a2\u30ce\u30b9", 
+    "Giaprey (\u30ae\u30a2\u30ce\u30b9)"
+  ], 
+  [
+    "\u30b2\u30cd\u30dd\u30b9", 
+    "Genprey (\u30b2\u30cd\u30dd\u30b9)"
+  ], 
+  [
+    "\u30a4\u30fc\u30aa\u30b9", 
+    "Ioprey (\u30a4\u30fc\u30aa\u30b9)"
+  ], 
+  [
+    "\u30b8\u30e3\u30ae\u30a3", 
+    "Jaggi (\u30b8\u30e3\u30ae\u30a3)"
+  ], 
+  [
+    "\u30de\u30c3\u30ab\u30a9", 
+    "Maccau (\u30de\u30c3\u30ab\u30a9)"
+  ], 
+  [
+    "\u30e9\u30f3\u30dd\u30b9", 
+    "Velociprey (\u30e9\u30f3\u30dd\u30b9)"
+  ], 
+  [
+    "\u30ac\u30df\u30b6\u30df", 
+    "Ceanataur (\u30ac\u30df\u30b6\u30df)"
+  ], 
+  [
+    "\u30e4\u30aa\u30b6\u30df", 
+    "Hermitaur (\u30e4\u30aa\u30b6\u30df)"
+  ], 
+  [
+    "\u30d6\u30e9\u30f3\u30b4", 
+    "Blango (\u30d6\u30e9\u30f3\u30b4)"
+  ], 
+  [
+    "\u30ac\u30a6\u30b7\u30ab", 
+    "Anteka (\u30ac\u30a6\u30b7\u30ab)"
+  ], 
+  [
+    "\u30e0\u30fc\u30d5\u30a1", 
+    "Mufa (\u30e0\u30fc\u30d5\u30a1)"
+  ], 
+  [
+    "\u30eb\u30c9\u30ed\u30b9", 
+    "Ludroth (\u30eb\u30c9\u30ed\u30b9)"
+  ], 
+  [
+    "\u30a2\u30a4\u30eb\u30fc", 
+    "Felyne (\u30a2\u30a4\u30eb\u30fc)"
+  ], 
+  [
+    "\u30e1\u30e9\u30eb\u30fc", 
+    "Melynx (\u30e1\u30e9\u30eb\u30fc)"
+  ], 
+  [
+    "\u30c7\u30eb\u30af\u30b9", 
+    "Delex (\u30c7\u30eb\u30af\u30b9)"
+  ], 
+  [
+    "\u30ac\u30d6\u30e9\u30b9", 
+    "Remobra (\u30ac\u30d6\u30e9\u30b9)"
+  ], 
+  [
+    "\u708e\u6208\u7adc", 
+    "Agnaktor (\u30a2\u30b0\u30ca\u30b3\u30c8\u30eb)"
+  ], 
+  [
+    "\u9752\u718a\u7363", 
+    "Arzuros (\u30a2\u30aa\u30a2\u30b7\u30e9)"
+  ], 
+  [
+    "\u96ea\u7345\u5b50", 
+    "Blangonga (\u30c9\u30c9\u30d6\u30e9\u30f3\u30b4)"
+  ], 
+  [
+    "\u87f9\u57ce\u58c1", 
+    "Spearbreaker Daimyo Hermitaur (\u77db\u7815\u30c0\u30a4\u30df\u30e7\u30a6\u30b6\u30b6\u30df)"
+  ], 
+  [
+    "\u6050\u66b4\u7adc", 
+    "Deviljho (\u30a4\u30d3\u30eb\u30b8\u30e7\u30fc)"
+  ], 
+  [
+    "\u71fc\u6ec5\u5203", 
+    "Incinerating Blade Dinovaldo (\u71fc\u6ec5\u5203\u30c7\u30a3\u30ce\u30d0\u30eb\u30c9)"
+  ], 
+  [
+    "\u5c3e\u69cc\u7adc", 
+    "Duramboros (\u30c9\u30dc\u30eb\u30d9\u30eb\u30af)"
+  ], 
+  [
+    "\u9ed2\u8755\u7adc", 
+    "Gore Magala (\u30b4\u30a2\u30fb\u30de\u30ac\u30e9)"
+  ], 
+  [
+    "\u8df3\u72d7\u7adc", 
+    "Great Maccau (\u30c9\u30b9\u30de\u30c3\u30ab\u30a9)"
+  ], 
+  [
+    "\u6bd2\u602a\u9ce5", 
+    "Gypceros (\u30b2\u30ea\u30e7\u30b9)"
+  ], 
+  [
+    "\u5947\u733f\u72d0", 
+    "Kecha Wacha (\u30b1\u30c1\u30e3\u30ef\u30c1\u30e3)"
+  ], 
+  [
+    "\u767d\u514e\u7363", 
+    "Lagombi (\u30a6\u30eb\u30af\u30b9\u30b9)"
+  ], 
+  [
+    "\u5927\u96ea\u4e3b", 
+    "Heavy Snow Lord Lagombi (\u5927\u96ea\u4e3b\u30a6\u30eb\u30af\u30b9\u30b9)"
+  ], 
+  [
+    "\u6eb6\u5ca9\u7adc", 
+    "Lavasioth (\u30f4\u30a9\u30eb\u30ac\u30ce\u30b9)"
+  ], 
+  [
+    "\u7d5e\u86c7\u7adc", 
+    "Najarala (\u30ac\u30e9\u30e9\u30a2\u30b8\u30e3\u30e9)"
+  ], 
+  [
+    "\u6f5c\u53e3\u7adc", 
+    "Nibelsnarf (\u30cf\u30d7\u30eb\u30dc\u30c3\u30ab)"
+  ], 
+  [
+    "\u9280\u706b\u7adc", 
+    "Silver Rathalos (\u30ea\u30aa\u30ec\u30a6\u30b9\u5e0c\u5c11\u7a2e)"
+  ], 
+  [
+    "\u96cc\u706b\u7adc", 
+    "Rathian (\u30ea\u30aa\u30ec\u30a4\u30a2)"
+  ], 
+  [
+    "\u91d1\u706b\u7adc", 
+    "Gold Rathian (\u30ea\u30aa\u30ec\u30a4\u30a2\u5e0c\u5c11\u7a2e)"
+  ], 
+  [
+    "\u5fb9\u7532\u866b", 
+    "Seltas (\u30a2\u30eb\u30bb\u30eb\u30bf\u30b9)"
+  ], 
+  [
+    "\u91cd\u7532\u866b", 
+    "Seltas Queen (\u30b2\u30cd\u30eb\u30fb\u30bb\u30eb\u30bf\u30b9)"
+  ], 
+  [
+    "\u5343\u5203\u7adc", 
+    "Seregios (\u30bb\u30eb\u30ec\u30ae\u30aa\u30b9)"
+  ], 
+  [
+    "\u6ce1\u72d0\u7adc", 
+    "Tamamitsune (\u30bf\u30de\u30df\u30c4\u30cd)"
+  ], 
+  [
+    "\u8352\u9264\u722a", 
+    "Ruinous Hook Claw Tigrex (\u8352\u9264\u722a\u30c6\u30a3\u30ac\u30ec\u30c3\u30af\u30b9)"
+  ], 
+  [
+    "\u8d64\u7532\u7363", 
+    "Volvidon (\u30e9\u30f3\u30b0\u30ed\u30c8\u30e9)"
+  ], 
+  [
+    "\u9ed2\u72fc\u9ce5", 
+    "Yian Garuga (\u30a4\u30e3\u30f3\u30ac\u30eb\u30eb\u30ac)"
+  ], 
+  [
+    "\u5316\u3051\u9bab", 
+    "Zamtrios (\u30b6\u30dc\u30a2\u30b6\u30ae\u30eb)"
+  ], 
+  [
+    "\u96f7\u72fc\u7adc", 
+    "Zinogre (\u30b8\u30f3\u30aa\u30a6\u30ac)"
+  ], 
+  [
+    "\u714c\u9ed2\u9f8d", 
+    "Alatreon (\u30a2\u30eb\u30d0\u30c8\u30ea\u30aa\u30f3)"
+  ], 
+  [
+    "\u30ad\u30ea\u30f3", 
+    "Kirin (\u30ad\u30ea\u30f3)"
+  ], 
+  [
+    "\u5929\u5efb\u9f8d", 
+    "Shagaru Magala (\u30b7\u30e3\u30ac\u30eb\u30de\u30ac\u30e9)"
+  ], 
+  [
+    "\u708e\u738b\u9f8d", 
+    "Teostra (\u30c6\u30aa\u30fb\u30c6\u30b9\u30ab\u30c8\u30eb)"
+  ], 
+  [
+    "\u30b1\u30eb\u30d3", 
+    "Kelbi (\u30b1\u30eb\u30d3)"
+  ], 
+  [
+    "\u96f2\u7f8a\u9e7f", 
+    "Mufa (\u30e0\u30fc\u30d5\u30a1)"
+  ], 
+  [
+    "\u8349\u98df\u7adc", 
+    "Rhenoplos (\u30ea\u30ce\u30d7\u30ed\u30b9)"
+  ], 
+  [
+    "\u9996\u9cf4\u7adc", 
+    "Rimosetosu (\u30ea\u30e2\u30bb\u30c8\u30b9)"
+  ], 
+  [
+    "\u5782\u76ae\u7adc", 
+    "Slagtoth (\u30ba\u30ef\u30ed\u30dd\u30b9)"
+  ], 
+  [
+    "\u6c34\u751f\u7363", 
+    "Ludroth (\u30eb\u30c9\u30ed\u30b9)"
+  ], 
+  [
+    "\u6eb6\u5ca9\u7363", 
+    "Uroktor (\u30a6\u30ed\u30b3\u30c8\u30eb)"
+  ], 
+  [
+    "\u98db\u7532\u866b", 
+    "Bnahabra (\u30d6\u30ca\u30cf\u30d6\u30e9)"
+  ], 
+  [
+    "\u8987\u7adc", 
+    "Akantor (\u30a2\u30ab\u30e0\u30c8\u30eb\u30e0)"
+  ], 
+  [
+    "\u7815\u7adc", 
+    "Brachydios (\u30d6\u30e9\u30ad\u30c7\u30a3\u30aa\u30b9)"
+  ], 
+  [
+    "\u5927\u732a", 
+    "Bulldrome (\u30c9\u30b9\u30d5\u30a1\u30f3\u30b4)"
+  ], 
+  [
+    "\u7802\u7adc", 
+    "Cephadrome (\u30c9\u30b9\u30ac\u30ec\u30aa\u30b9)"
+  ], 
+  [
+    "\u76fe\u87f9", 
+    "Daimyo Hermitaur (\u30c0\u30a4\u30df\u30e7\u30a6\u30b6\u30b6\u30df)"
+  ], 
+  [
+    "\u65ac\u7adc", 
+    "Dinovaldo (\u30c7\u30a3\u30ce\u30d0\u30eb\u30c9)"
+  ], 
+  [
+    "\u5de8\u7363", 
+    "Gamuto (\u30ac\u30e0\u30fc\u30c8)"
+  ], 
+  [
+    "\u591c\u9ce5", 
+    "Hororohoruru (\u30db\u30ed\u30ed\u30db\u30eb\u30eb)"
+  ], 
+  [
+    "\u6d77\u7adc", 
+    "Lagiacrus (\u30e9\u30ae\u30a2\u30af\u30eb\u30b9)"
+  ], 
+  [
+    "\u8fc5\u7adc", 
+    "Nargacuga (\u30ca\u30eb\u30ac\u30af\u30eb\u30ac)"
+  ], 
+  [
+    "\u6c34\u7adc", 
+    "Plesioth (\u30ac\u30ce\u30c8\u30c8\u30b9)"
+  ], 
+  [
+    "\u96fb\u7adc", 
+    "Raizekusu (\u30e9\u30a4\u30bc\u30af\u30b9)"
+  ], 
+  [
+    "\u706b\u7adc", 
+    "Rathalos (\u30ea\u30aa\u30ec\u30a6\u30b9)"
+  ], 
+  [
+    "\u6c34\u7363", 
+    "Royal Ludroth (\u30ed\u30a2\u30eb\u30c9\u30ed\u30b9)"
+  ], 
+  [
+    "\u938c\u87f9", 
+    "Shogun Ceanataur (\u30b7\u30e7\u30a6\u30b0\u30f3\u30ae\u30b6\u30df)"
+  ], 
+  [
+    "\u9b3c\u86d9", 
+    "Tetsucabra (\u30c6\u30c4\u30ab\u30d6\u30e9)"
+  ], 
+  [
+    "\u8f5f\u7adc", 
+    "Tigrex (\u30c6\u30a3\u30ac\u30ec\u30c3\u30af\u30b9)"
+  ], 
+  [
+    "\u5d29\u7adc", 
+    "Ukanlos (\u30a6\u30ab\u30e0\u30eb\u30d0\u30b9)"
+  ], 
+  [
+    "\u5b9d\u7e8f", 
+    "Treasure Clad Uragaan (\u5b9d\u7e8f\u30a6\u30e9\u30ac\u30f3\u30ad\u30f3)"
+  ], 
+  [
+    "\u602a\u9ce5", 
+    "Yian Kut-Ku (\u30a4\u30e3\u30f3\u30af\u30c3\u30af)"
+  ], 
+  [
+    "\u96bb\u773c", 
+    "One-Eyed Yian Garuga (\u96bb\u773c\u30a4\u30e3\u30f3\u30ac\u30eb\u30eb\u30ac)"
+  ], 
+  [
+    "\u5d50\u9f8d", 
+    "Amatsumagatsuchi (\u30a2\u30de\u30c4\u30de\u30ac\u30c4\u30c1)"
+  ], 
+  [
+    "\u971e\u9f8d", 
+    "Chameleos (\u30aa\u30aa\u30ca\u30ba\u30c1)"
+  ], 
+  [
+    "\u5e7b\u7363", 
+    "Kirin (\u30ad\u30ea\u30f3)"
+  ], 
+  [
+    "\u92fc\u9f8d", 
+    "Kushala Daora (\u30af\u30b7\u30e3\u30eb\u30c0\u30aa\u30e9)"
+  ], 
+  [
+    "\u9ab8\u9f8d", 
+    "Osutogaroa (\u30aa\u30b9\u30c8\u30ac\u30ed\u30a2)"
+  ], 
+  [
+    "\u4e38\u9ce5", 
+    "Gargwa (\u30ac\u30fc\u30b0\u30a1)"
+  ], 
+  [
+    "\u72d7\u7adc", 
+    "Jaggi (\u30b8\u30e3\u30ae\u30a3)"
+  ], 
+  [
+    "\u72d7\u7adc", 
+    "Jaggia (\u30b8\u30e3\u30ae\u30a3\u30ce\u30b9)"
+  ], 
+  [
+    "\u30e2\u30b9", 
+    "Mosswine (\u30e2\u30b9)"
+  ], 
+  [
+    "\u30dd\u30dd", 
+    "Popo (\u30dd\u30dd)"
+  ], 
+  [
+    "\u7532\u866b", 
+    "Altaroth (\u30aa\u30eb\u30bf\u30ed\u30b9)"
+  ], 
+  [
+    "\u76fe\u866b", 
+    "Konchu (\u30af\u30f3\u30c1\u30e5\u30a6)"
+  ]
+];
---
+++ translate_links.js
@@ -0,0 +1,52 @@
+var replace_list = [];
+for (var i = 0; i < stree_list.length; i++) {
+    var stree = stree_list[i];
+    var name = stree["name"];
+    var name_jp = stree["name_jp"];
+    var name_both = name + " (" + name_jp + ")";
+    replace_list.push([name_jp, name_both]);
+}
+// sort longest first
+replace_list.sort(function compareElement0Length(a, b) {
+    return b[0].length - a[0].length;
+});
+
+var tag, tag_text, match_text;
+var matched = false;
+var no_idx, tag_title;
+var atags = document.getElementsByTagName("a");
+for (var i = 0; i < atags.length; i++) {
+    tag = atags[i];
+    matched = false;
+    tag_text = tag.innerText;
+    for (var j=0; j<replace_list.length; j++) {
+        // replace on exact match only
+        if (tag_text == replace_list[j][0]) {
+            tag.innerText = replace_list[j][1];
+            matched = true;
+            break;
+        }
+    }
+    if (matched) {
+        continue;
+    }
+
+    // if the no kanji is in the link, try matching a monster title or monster
+    // name to the prefix before the no, and if found add a title attribute
+    // with the monster name.
+    no_idx = tag_text.indexOf("\u306e");
+    if (no_idx == -1) {
+        continue;
+    }
+    tag_title = tag_text.substr(0, no_idx);
+    for (var j=0; j<monster_replace_list.length; j++) {
+        // replace on match before 'no' kanji only
+        match_text = monster_replace_list[j][0];
+        if (tag_title == monster_replace_list[j][0]) {
+            console.log("matched monster, setting title attr " + tag_title);
+            tag.setAttribute("title", monster_replace_list[j][1]);
+            matched = true;
+            break;
+        }
+    }
+}
