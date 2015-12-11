13a70c9f6c00b6d2c6dba31aca52b1c4bb5512f3
Bryce Allen <ballen@ci.uchicago.edu>
2015-12-11 02:51:41 -0600

    add selection and context menu re-enabler

---
+++ fix_select_and_menu.js
@@ -0,0 +1,11 @@
+// Remove obnixous event handlers that disable selection and
+// context menu. Note that this can't be done directly in the
+// content script, because it runs in isolated context and can't
+// see the event listener properties set by the main page.
+// Inserting the script into the body of the page allows it to
+// access the values and remove the offending handlers.
+var script = document.createElement('script');
+script.type = "text/javascript";
+script.innerHTML = "document.body.onselectstart = null; "
+                 + "document.body.oncontextmenu = null;";
+document.body.appendChild(script);
